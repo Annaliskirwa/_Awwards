a78329504c08eeb500e48f71db448801a50690d8
Annaliskirwa <annaliskirwa@gmail.com>
2021-12-13 20:13:40 +0300

    Creates the profile model class

--- awards/models.py
+++ awards/models.py
@@ -3,3 +3,10 @@ from cloudinary.models import CloudinaryField
 from django.contrib.auth.models import User
 
 # Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete = models.CASCADE)
+    profile_pic = CloudinaryField('Profile Picture')
+    bio =  models.TextField()
+    location = models.CharField(max_length = 40)
+    email = models.EmailField()
+    link = models.URLField()
