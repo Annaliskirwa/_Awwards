a74381461a090d9f9050d848562ca91bbd8c45fa
Annaliskirwa <annaliskirwa@gmail.com>
2021-12-13 20:29:53 +0300

    Created the project models class

--- awards/models.py
+++ awards/models.py
@@ -23,3 +23,16 @@ class Profile(models.Model):
     def edit_bio(self, new_bio):
         self.bio = new_bio
         self.save()
+
+class Project(models.Model):
+    name = models.CharField(max_length = 30)
+    screenshot = CloudinaryField('Project screenshot')
+    description = models.TextField()
+    link = models.URLField()
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    voters = models.IntegerField(default=0)
+    post_date = models.DateTimeField(auto_now_add=True, null = True)
+    average_design = models.FloatField(default=0,)
+    average_usability = models.FloatField(default=0)
+    average_content = models.FloatField(default=0)
+    average_score = models.FloatField(default=0)
