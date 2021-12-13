7d782b12d6a31080f1636fb34fb8e4ccc9f28920
Annaliskirwa <annaliskirwa@gmail.com>
2021-12-13 21:44:21 +0300

    Changing the base url

--- awards/urls.py
+++ awards/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns=[
-    url('^$',views.welcome,name = 'welcome'),
+    # url('^$',views.welcome,name = 'welcome'),
     url(r'^$',views.home,name = 'home'),
     url(r'^email/$',views.email,name = 'email'),
     url(r'^create_profile/$',views.create_profile,name = 'create_profile'),
