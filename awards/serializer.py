7030b3df6aebd7c82ed77dcec6fa3cc879652d46
Annaliskirwa <annaliskirwa@gmail.com>
2021-12-14 20:46:03 +0300

    Profile serializer

--- awards/serializer.py
+++ awards/serializer.py
@@ -5,3 +5,9 @@ class ProjectSerializer(serializers.ModelSerializer):
     class Meta :
         model = Project
         fields = ('name', 'description','screenshot', 'link', 'profile', 'post_date', 'average_design', 'average_content', 'average_score', 'average_usability')
+
+
+class ProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Profile
+        fields = ('user', 'bio', 'profile_pic', 'email','location', 'link')
