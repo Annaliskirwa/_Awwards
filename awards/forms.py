16817a95778e84eac84615ca9ee42e47fd4a0f65
Annaliskirwa <annaliskirwa@gmail.com>
2021-12-14 09:20:33 +0300

    Add project form

--- awards/forms.py
+++ awards/forms.py
@@ -6,3 +6,8 @@ class CreateProfileForm(ModelForm):
     class Meta:
         model = Profile
         exclude = ['user']
+
+class AddProjectForm(ModelForm):
+    class Meta:
+        model = Project
+        exclude = ['profile', 'post_date', 'voters', 'design_score','usability_score','content_score','average_design','average_usability','average_content','average_score']
