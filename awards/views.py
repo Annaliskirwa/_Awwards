7e2bfd5e74d355669ac0703328db4ba13cd6c9c0
Annaliskirwa <annaliskirwa@gmail.com>
2021-12-14 08:53:10 +0300

    importing packages needed by views

--- awards/views.py
+++ awards/views.py
@@ -1,5 +1,16 @@
-from django.shortcuts import render
+import datetime as dt
+import statistics
+
+from django.shortcuts import render, redirect
 from django.http  import HttpResponse
+from django.contrib.auth.decorators import login_required
+from .forms import AddProjectForm, RateProjectForm, CreateProfileForm
+from .email import send_signup_email
+from django.contrib.auth.models import User
+from .models import Profile, Project, Vote
+from django.core.exceptions import ObjectDoesNotExist
+from django.http import HttpResponseRedirect, Http404
+from django.urls import reverse
 
 # Create your views here.
 def welcome(request):
