e8d54aa411e12a0fe3fdd78e144d3ad56f7af0ca
Annaliskirwa <annaliskirwa@gmail.com>
2021-12-14 09:22:49 +0300

    Rate project form

--- awards/forms.py
+++ awards/forms.py
@@ -11,3 +11,8 @@ class AddProjectForm(ModelForm):
     class Meta:
         model = Project
         exclude = ['profile', 'post_date', 'voters', 'design_score','usability_score','content_score','average_design','average_usability','average_content','average_score']
+
+class RateProjectForm(ModelForm):
+    class Meta:
+        model = Vote
+        exclude = ['post_date', 'voter', 'project'] 
