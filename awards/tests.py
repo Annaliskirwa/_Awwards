e31e8457fb41f9978ff0e2483ab5dd065d996f0e
Annaliskirwa <annaliskirwa@gmail.com>
2021-12-14 21:21:05 +0300

    Project create, delete,display and search test cases

--- awards/tests.py
+++ awards/tests.py
@@ -22,3 +22,46 @@ class ProfileTestClass(TestCase):
     def test_edit_bio(self):
         self.profile.edit_bio('I am a software engineer')
         self.assertEqual(self.profile.bio, 'I am a software engineer')
+
+
+class ProjectTestClass(TestCase):
+    def setUp(self):
+        self.Annalis = User(username = "Annalis", email = "annaliskirwa@gmail.com",password = "Ann")
+        self.profile = Profile(user= self.Annalis, profile_pic='mepng',bio='bio', location='Nairobi, Kenya', email='annaliskirwa@gmail', link='www.ann.com')
+        self.project = Project(name= "test", screenshot = "imageurl", description ="test project", link = "testlink", profile= self.profile)
+
+        self.Annalis.save()
+        self.profile.save_profile()
+        self.project.save_project()
+
+    def tearDown(self):
+        Profile.objects.all().delete()
+        User.objects.all().delete()
+        Project.objects.all().delete()
+
+    def test_image_instance(self):
+        self.assertTrue(isinstance(self.project, Project))
+
+    def test_save_project(self):
+        projects = Project.objects.all()
+        self.assertTrue(len(projects)> 0)
+
+    def test_delete_project(self):
+        projects1 = Project.objects.all()
+        self.assertEqual(len(projects1),1)
+        self.project.delete_project()
+        projects2 = Project.objects.all()
+        self.assertEqual(len(projects2),0)
+
+    def test_display_projects(self):
+        projects = Project.display_all_projects()
+        self.assertTrue(len(projects) > 0 )
+
+    def test_search_project(self):
+        project = Project.search_project('test')
+        self.assertEqual(len(project),1)
+
+    def test_get_user_projects_(self):
+        profile_projects = Project.get_user_projects(self.profile.id)
+        self.assertEqual(profile_projects[0].name, 'test')
+        self.assertEqual(len(profile_projects),1 )
